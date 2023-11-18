22a02a04d2798827d5073dae19b9f99fbfd4d94f
gbdev03 <gbdev03@gbringel.com>
2023-11-17 23:48:24 -0400

    feat: test com lib de responsividade checada, não foi possível validar com tags do prime react;

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import SecondSection from './components/LuvasDeBox.vue';
 import ThirdSection from './components/ThirdSection.vue';
 import FourthSection from './components/FourthSection.vue';
 import FivetiSection from './components/FivetiSection.vue';
-
+import AnimateOnScroll from 'primevue/animateonscroll';
 export default{
   components:{
     FirstSection,
@@ -13,7 +13,8 @@ export default{
     ThirdSection,
     FourthSection,
     FivetiSection,
-    ScrollPanel
+    ScrollPanel,
+    AnimateOnScroll
   }
 }
 </script>
@@ -21,11 +22,40 @@ export default{
 <template>
 
   <div class="container-principal">
+
     <FirstSection></FirstSection>
     <SecondSection></SecondSection>
     <ThirdSection></ThirdSection>
     <FourthSection></FourthSection>
     <FivetiSection></FivetiSection>
+
+<!--    <div v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"-->
+<!--         class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-5000">-->
+<!--      <span class="text-3xl font-bold">fade-in</span>-->
+<!--    </div>-->
+
+<!--    <div v-animateonscroll="{ enterClass: 'fadeinleft', leaveClass: 'fadeoutleft' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-1000 animation-ease-in-out">-->
+<!--      <span class="text-3xl font-bold">fade-left</span>-->
+<!--    </div>-->
+
+<!--    <div v-animateonscroll="{ enterClass: 'fadeinright', leaveClass: 'fadeoutright' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-1000 animation-ease-in-out">-->
+<!--      <span class="text-3xl font-bold">fade-right</span>-->
+<!--    </div>-->
+
+<!--    <div v-animateonscroll="{ enterClass: 'zoomin', leaveClass: 'fadeout' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-1000">-->
+<!--      <span class="text-3xl font-bold">zoom</span>-->
+<!--    </div>-->
+<!--    <div v-animateonscroll="{ enterClass: 'flipleft', leaveClass: 'fadeout' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-1000 animation-ease-in-out">-->
+<!--      <span class="text-3xl font-bold">flip-left</span>-->
+<!--    </div>-->
+
+<!--    <div v-animateonscroll="{ enterClass: 'flipup', leaveClass: 'fadeout' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem border-round animation-duration-1000 animation-ease-in-out">-->
+<!--      <span class="text-3xl font-bold w-10rem">flip-y</span>-->
+<!--    </div>-->
+<!--    <div class="h-30rem"></div>-->
+<!--    <div v-animateonscroll="{ enterClass: 'scalein', leaveClass: 'fadeout' }" class="flex box shadow-4 justify-content-center align-items-center h-10rem w-10rem sm:h-15rem sm:w-15rem border-round animation-duration-1000 animation-ease-in-out">-->
+<!--      <span class="text-3xl font-bold">scalein</span>-->
+<!--    </div>-->
   </div>
 
 </template>
@@ -34,4 +64,41 @@ export default{
 .container-principal {
   overflow-x: hidden;
 }
+@keyframes slidedown-icon {
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(20px);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.slidedown-icon {
+  animation: slidedown-icon;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+}
+
+.box {
+  background-image: radial-gradient(var(--primary-300), var(--primary-600));
+  border-radius: 50% !important;
+  color: var(--primary-color-text);
+}
+
+.card {
+  background: var(--surface-card);
+  padding: 2rem;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+}
+
+p {
+  line-height: 1.75;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 
 import './style.css'
-import 'primevue/resources/themes/lara-light-teal/theme.css'
-import PrimeVue from 'primevue/config/config.esm'
+
+import PrimeVue from 'primevue/config'
+import ConfirmationService from 'primevue/confirmationservice';
+
 
 import App from './App.vue'
 import { createApp } from 'vue';
 
-
+import AnimateOnScroll from 'primevue/animateonscroll';
+import Tooltip from 'primevue/tooltip'
+import BadgeDirective from 'primevue/badgedirective'
+import Ripple from 'primevue/ripple'
+import StyleClass from 'primevue/styleclass'
+import FocusTrap from 'primevue/focustrap'
+import ToastService from 'primevue/toastservice'
+import DialogService from 'primevue/dialogservice'
+import './assets/theme_lara_claro.css'
 
 const app = createApp(App);
-app.use(PrimeVue);
+app.use(PrimeVue, { ripple: true  });
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+app.directive('animateonscroll', AnimateOnScroll);
+app.directive('tooltip', Tooltip);
+app.directive('badge', BadgeDirective);
+app.directive('ripple', Ripple);
+app.directive('styleclass', StyleClass);
+app.directive('focustrap', FocusTrap);
+app.directive('animateonscroll', AnimateOnScroll);
+
 app.mount("#app")
